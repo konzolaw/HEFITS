ce357e4ce463fe93dbec4137dc8534267be2d4af
konzolaw <125367223+konzolaw@users.noreply.github.com>
2024-03-04 17:57:00 +0300

    Added vital_signs form

---
+++ Vitalsigns.vb
@@ -0,0 +1,33 @@
+﻿Public Class Vitalsigns
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Heartbeat_box.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Bloodpressure_box.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Weight_box.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles weight_list.SelectedIndexChanged
+
+    End Sub
+End Class
